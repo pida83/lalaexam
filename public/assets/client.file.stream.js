8415a4ee945a79dc327249350fcfc6b0f4951976
PC <pida83@naver.com>
2020-03-13 18:39:40 +0900

    setting

--- public/assets/client.file.stream.js
+++ public/assets/client.file.stream.js
@@ -4,8 +4,82 @@ var fileStream = {
     init : function()
     {
         this.connector.on("connect", function(){
+            let chunks = {data: []};
+            let temp   = [];
+            let cnt    = 0;
+            // receive data
+            //let video = document.getElementsByTagName("video");
+            ss(fileStream.connector).emit("streamMp4",fileStream.stream,"data");
+            fileStream.stream.on('data', function(chunk) {
+                cnt++;
+                // 소스 태그 하나씩 넣는거는 비효율적인거 같아서 잘라서 넣어본다 ==> 서버에서 묶어서 하나씩 내려주도록 변경 해야겠다.
+                if (temp.length < 10) {
+                    temp.push(chunk)
+                } else {
+                    chunks.data.push(URL.createObjectURL(new Blob(temp)));
+                    temp = [];
+                }
+            });
+
+            fileStream.stream.on("finish",function(){
+                console.dir(chunks.data)
+                console.log(cnt)
+
+                let video = document.getElementsByTagName("video")[0];
+
+                /*
+                for (index in chunks.data) {
+                    var source = document.createElement("sources");
+
+                    console.log(chunks.data[index])
+                    //source.setAttribute("src",chunks.data[index]);
+                    //source.setAttribute("type","video/mp4");
+                    video.setAttribute("src",chunks.data[index])
+
+
+                    console.log(source.src)
+                    //document.body.getElementsByTagName("video").appendChild(source);
+                    $(video).append(source);
+                }
+                 */
+
+                $(video).on("play",function() {
+
+                });
+
+                $(video).on("ended",function() {
+                    console.log("end")
+                });
+
+                 //source.src = URL.createObjectURL(new Blob(chunk));
+                 //$(video).append(source);
+
+
+                //var video = document.createElement("video");
+                //var video = document.getElementsByTagName("video");
+                //video[0].src = URL.createObjectURL(new Blob(chunks));
+                //document.body.appendChild(video);
+
+                // var img = document.createElement("img");
+                // img.src = URL.createObjectURL(new Blob(chunks));
+                // console.log(img.src)
+                // document.body.appendChild(img);
+
+
+
+                // let videoData =URL.createObjectURL(new Blob(chunks));
+                // $("#movv").attr("src",videoData);
+                // console.log($("#movv").attr("src"))
+
+                //document.body.appendChild(img);
+                //img.src = URL.createObjectURL(new Blob(chunks));
+                //document.body.appendChild(img);
+            })
         });
 
+
+    },
+    addPlayList : function() {
 
     },
     sendFile : function()
@@ -22,7 +96,6 @@ var fileStream = {
             blobStream.on('data', function(chunk) {
                 size += chunk.length;
                 var uploadPercent = Math.floor(size / file.size * 100) + '%';
-                console.dir(uploadPercent)
                 if (size === file.size) {
                     fileStream.connector.emit("after upload",function(fileName) {
                         $('#user_profile').attr("src","/upload/"+fileName).show();
